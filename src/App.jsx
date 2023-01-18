d741c748031287342b4e72ec443a81a7f1a8d0e5
martin0123478 <martin_agui97@outlook.es>
2023-01-17 19:48:37 -0600

    Creando arquitectura y primeros componentes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+function App() {
+
+
+  return (
+    <>
+    
+     <Header/>
+     <Formulario/>
+     <ListadoPacientes/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+
+
+function Formulario() {
+  return (
+    <h1>Formulario</h1>
+  )
+}
+
+export default Formulario
