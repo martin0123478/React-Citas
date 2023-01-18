9bf7c8de6a3cb81f5196265ed5af37f78021694d
martin0123478 <martin_agui97@outlook.es>
2023-01-18 12:21:22 -0600

    Creando el formulario y agregandole clases

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 function ListadoPacientes(props) {
   return (
-    <h2>Listado Pacientes</h2>
+    <div className="w-1/2 lg:w-3/5">
+        <h2>Listado Pacientes</h2>
+    </div>
+   
   )
 }
 
