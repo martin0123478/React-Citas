591f7c012a1130ff3a9f1be786fd420ef49cffe6
martin0123478 <martin_agui97@outlook.es>
2023-01-20 17:32:53 -0600

    Agregando estilos a la lista de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Paciente from './Paciente'
 
 
 function ListadoPacientes(props) {
   return (
-    <div className="w-1/2 lg:w-3/5">
-        <h2>Listado Pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+        <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+        <p className='text-xl mt-5 mb-10 text-center'>
+          Adminstra tus {''}
+          <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+        </p>
+        <Paciente/>
+        <Paciente/>
+        <Paciente/>
+        <Paciente/>
+        <Paciente/>
+        <Paciente/>
     </div>
    
   )
