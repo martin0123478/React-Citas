af818cf95a4a89b134e00496ddf47f8c2298572c
martin0123478 <martin_agui97@outlook.es>
2023-01-23 18:01:22 -0600

    Validando formulario y mostrando error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,13 +6,20 @@ function Formulario() {
   const [email,setEmail] = useState('')
   const [fecha,setFecha] = useState('')
   const [sintomas,setSintomas] = useState('')
+  const [error,setError] = useState(false)
 
 
 
 
   const handleSubmit = (e) =>{
     e.preventDefault()
-    console.log('enviando formulario')
+    if([nombre,propietario,email,fecha,sintomas].includes('')){
+      console.log('Hay al menos un campo vacio')
+      setError(true)
+      return
+    }
+    setError(false)
+   
   }
   
   return (
@@ -27,6 +34,13 @@ function Formulario() {
       onSubmit={handleSubmit}
       className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
+          {error &&(
+            <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3
+            rounded'>
+              <p>Todos los campos son obligatiorios</p>
+            </div>
+            
+          )}
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
             Nombre Mascota</label>
           <input id="mascota" type="text" 
